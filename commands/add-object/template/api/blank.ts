558f8269996896f2a3c96219eee0e5f2fd21b2b1
Khiem Nguyen <143854493+khiemhuu@users.noreply.github.com>
2026-06-01 14:51:55 +1000

    Clean repo

--- commands/add-object/template/api/blank.ts
+++
@@ -1,7 +0,0 @@
-import { {{ nameFirst }} } from '@baseline/types/{{ nameKebab }}';
-
-export const {{ nameCamel }}Mapper = (data: {{ nameFirst }}): {{ nameFirst }} => {
-  const {{ nameCamel }}: {{ nameFirst }} = {{{ mapperFields }}
-  };
-  return {{ nameCamel }};
-};
--- commands/add-object/template/types/blank.d.ts
+++
@@ -1,3 +0,0 @@
-export interface {{ nameFirst }} {
-{{ typeFields }}
-}
